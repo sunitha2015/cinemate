3c4fce2a23c66e0cf6d356ec78c626c8ebc87171
sunitha <sunitha@igdst.com>
2025-08-05 20:04:16 +0530

    Update Run Build Error

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,11 +1,11 @@
 "use client"
 import { Suspense } from "react";
-import SearchMoviesPage from "@/components/SearchMoviesPage"; // or wherever it's located
+import SearchMoviesPage from "@/components/SearchMoviesPage"; 
 
 export default function search() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <SearchMoviesPage apiPath={"movie/search"} />
+      <SearchMoviesPage apiPath={"search/movie"} />
     </Suspense>
   );
 }
