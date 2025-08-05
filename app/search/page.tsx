5b67bb45a179c90fea06ba0d3ab60398448989b1
sunitha <sunitha@igdst.com>
2025-08-05 19:53:07 +0530

    Update Run Build Error

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,35 +1,11 @@
-'use client';
+"use client"
+import { Suspense } from "react";
+import SearchMoviesPage from "@/components/SearchMoviesPage"; // or wherever it's located
 
-import { useFetch } from "@/hooks/useFetch";
-import MovieCard from "@/components/MovieCard";
-import { useSearchParams } from "next/navigation";
-
-type MovieListProps = {
-  apiPath: string;
-};
-
-export default function SearchMoviesPage({ apiPath = "search/movie" }: MovieListProps) {
-  const searchParams = useSearchParams();
-  const queryTerm = searchParams.get("q") || "";
-
-  const { data: movies = [] } = useFetch(apiPath, queryTerm);
-
-
+export default function search() {
   return (
-    <section className="p-4">
-      {movies.length === 0 ? (
-        <p className="text-center text-gray-500 text-3xl">No results found for "{queryTerm}"</p>
-      ) : (
-        <div>
-          <p className="text-center text-gray-500 text-3xl my-6">Results for "{queryTerm}"</p>
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-            {movies.map((movie, index) => (
-              <MovieCard key={index} movie={movie} />
-            ))}
-          </div>
-        </div>
-
-      )}
-    </section>
+    <Suspense fallback={<div>Loading...</div>}>
+      <SearchMoviesPage apiPath={"movie/search"} />
+    </Suspense>
   );
 }
---
+++ components/SearchMoviesPage.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import { useFetch } from "@/hooks/useFetch";
+import MovieCard from "@/components/MovieCard";
+import { useSearchParams } from "next/navigation";
+type MovieListProps = {
+  apiPath: string;
+};
+export default function SearchMoviesPage({ apiPath }: MovieListProps) {
+  const searchParams = useSearchParams();
+  const queryTerm = searchParams.get("q") || "";
+
+  const { data: movies = [] } = useFetch(apiPath, queryTerm);
+
+  return (
+    <section className="p-4">
+      {movies.length === 0 ? (
+        <p className="text-center text-gray-500 text-3xl">
+          No results found for "{queryTerm}"
+        </p>
+      ) : (
+        <div>
+          <p className="text-center text-gray-500 text-3xl my-6">
+            Results for "{queryTerm}"
+          </p>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+            {movies.map((movie, index) => (
+              <MovieCard key={index} movie={movie} />
+            ))}
+          </div>
+        </div>
+      )}
+    </section>
+  );
+}
